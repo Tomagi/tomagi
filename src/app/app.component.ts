ea05d6bea585822c643310c14264667d75c5bbe7
tomergueta <tomer@stellares.ai>
2020-05-26 11:32:10 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, TemplateRef, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tomagi';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  condition: string = null;
+
+  @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
+  @ViewChild(TemplateRef) template: TemplateRef<any>;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.condition = 'Resolved!!';
+    }, 3000);
+  }
+
+  ngAfterViewInit(): void {
+    this.container.createEmbeddedView(this.template);
+  }
 }
